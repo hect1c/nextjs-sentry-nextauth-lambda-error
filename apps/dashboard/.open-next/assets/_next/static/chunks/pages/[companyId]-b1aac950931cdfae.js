8d3bd9fada2795c51e0e7ea2691077d19a64b749
hartley <hartley@palqee.com>
2023-04-08 14:17:55 +0100

    remove .open-next folder

--- apps/dashboard/.open-next/assets/_next/static/chunks/pages/[companyId]-b1aac950931cdfae.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[747],{1545:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[companyId]",function(){return t(1657)}])},1657:function(n,u,t){"use strict";t.r(u);var _=t(2322);let e=()=>(0,_.jsx)(_.Fragment,{children:"Company"});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1545)}),_N_E=n.O()}]);
-//# sourceMappingURL=[companyId]-b1aac950931cdfae.js.map
